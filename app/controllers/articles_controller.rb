59d54c92a08bc2041e6eb1a545ee54b3c4a39d5b
tahara <a@a.com>
2015-10-09 05:12:23 +0900

    削除機能実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -40,6 +40,13 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	# DELETE /articles/:id
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+		redirect_to root_path, notice: '削除に成功しました'
+	end
+
 	private
 		def article_params
 			params.require(:article).permit(:title, :content)
