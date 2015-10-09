ae1eaba25898a87038950aec25848d9c70b23e87
tahara <a@a.com>
2015-10-09 11:51:27 +0900

    Markdown機能追加

--- Gemfile
+++ Gemfile
@@ -48,3 +48,4 @@ end
 gem "twitter-bootstrap-rails"
 gem "slim-rails"
 gem 'bootstrap_form'
+gem 'redcarpet'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+	@@markdown = Redcarpet::Markdown.new Redcarpet::Render::HTML,
+		autolink: true, #http, https, ftp等の自動リンク
+		space_after_headers: true, #「#」のあとに空白を必要とするか
+		no_intra_emphasis: true, # foo_bar_bazをemタグにしない
+		fenced_code_blocks: true, # 「```」によるコードブロック
+		tables: true, # PHP-Markdownスタイルの表を有効化
+		strikethrough: true # ~~」によるdelタグの生成
+
+	def markdown(text)
+		@@markdown.render(text).html_safe
+	end
 end
