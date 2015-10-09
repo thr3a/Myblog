e1dd38a1fab2b00506f435fb7b6236a6b9978d99
tahara <a@a.com>
2015-10-09 16:53:02 +0900

    カラムcategoryの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
 	validates :title, presence: true, length: { maximum: 30 }
 	validates :content, presence: true
+	validates :category, presence: true, numericality: { only_integer: true }
 end
