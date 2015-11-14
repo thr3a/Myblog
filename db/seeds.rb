7bdc9f6adeabe80394dc1f9cc6ad24d2c3ff72de
thr3a <a@a.com>
2015-11-14 21:16:03 +0900

    他のカテゴリも適当に追加してみる

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Category.create(name: "その他")
+Category.create(name: "Ruby")
+Category.create(name: "Javascript")
+Category.create(name: "ねこ")
+
+
 Article.create(title: "no title", content: "hello world!", category_id: 1, author: "1179190262")
